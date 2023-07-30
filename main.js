358c3e85f389865686b5aefdf0317c944c140044
Salma Aktar Bithy <76905965+BithySalma@users.noreply.github.com>
2023-07-30 14:30:44 +0600

    updated

--- main.js
+++ main.js
@@ -1,2 +1,24 @@
-console.log("gfj")
-console.log("Bithy")
+
+
+
+//Asg=2
+
+let originalArray= [12, 34, 45, 23, 6, 78, 54, 90];
+let filteredEvenNumbers = originalArray.filter(n=>n%2==0);
+
+
+console.log("OriginalArray:" + originalArray);
+console.log(filteredEvenNumbers);
+
+
+//Asg=3
+
+function table(number){
+    for(var i=1;i<=10;i++){
+        var result = i*number;
+        console.log(`${number}x ${i} = ${result}`);
+    }
+}
+table(14);
+
+
